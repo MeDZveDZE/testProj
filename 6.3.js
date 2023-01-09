a0a4d18a465fea82826a48daa5d5d7c08137b883
MeDZveDZE <122085383+MeDZveDZE@users.noreply.github.com>
2023-01-09 12:19:38 +0300

    upd

--- 6.3.js
+++ 6.3.js
@@ -1,4 +1,6 @@
-const x = new B("Anna", 29, "Engineer", 'Minsk');
+//- Ещё в одном файле создать экземпляр класса В и вызвать метод класса A и класса B
+
+const x = new B("Anna", 29, "Engineer", "Minsk");
 x.nameAndAge();
 x.nameAndProfession();
 x.personIsFrom();
